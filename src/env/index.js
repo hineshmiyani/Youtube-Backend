340ca5b951e54ee104f1014896117ebb53f7eeff
Hinesh Miyani <hineshmiyani@gmail.com>
2024-02-15 11:09:21 +0530

    feat: add user and video model

--- src/env/index.js
+++ src/env/index.js
@@ -2,6 +2,10 @@ const ENV = {
   PORT: process.env.PORT || 8000,
   MONGODB_URL: process.env.MONGODB_URL,
   CORS_ORIGIN: process.env.CORS_ORIGIN,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
 };
 
 export default ENV;
---
+++ src/models/user.model.js
@@ -0,0 +1,85 @@
+import { Schema, model } from "mongoose";
+import jwt from "jwt";
+import bcrypt from "bcrypt";
+import ENV from "../env";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+      index: true,
+      required: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!"],
+    },
+    refreshToken: {
+      type: String,
+    },
+    watchHistory: [{ type: Schema.Types.ObjectId, ref: "Video" }],
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  const payload = {
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    fullName: this.fullName,
+  };
+
+  const token = await jwt.sign(payload, ENV.ACCESS_TOKEN_SECRET, {
+    expiresIn: ENV.ACCESS_TOKEN_EXPIRY,
+  });
+
+  return token;
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+  const payload = {
+    _id: this._id,
+  };
+
+  const token = await jwt.sign(payload, ENV.REFRESH_TOKEN_SECRET, {
+    expiresIn: ENV.REFRESH_TOKEN_EXPIRY,
+  });
+
+  return token;
+};
+
+export const User = model("User", userSchema);
