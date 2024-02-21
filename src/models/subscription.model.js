afcdc47edcafb002fe5173ca7d44a3f21147a2a2
Hinesh Miyani <hineshmiyani@gmail.com>
2024-02-21 21:38:39 +0530

    feat: add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require("mongoose");
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = model("Subscription", subscriptionSchema);
