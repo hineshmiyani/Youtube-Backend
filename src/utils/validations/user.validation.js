37165bed250cf94a6946a24835a83dd26fe90bb2
Hinesh Miyani <hineshmiyani@gmail.com>
2024-03-07 10:41:36 +0530

    chore: update user validation schema

--- src/utils/validations/user.validation.js
+++ src/utils/validations/user.validation.js
@@ -1,15 +1,12 @@
 import { z } from "zod";
 
 const userSchema = z.object({
-  username: z.string().transform((val) => val.toLowerCase().trim()),
-  email: z
-    .string()
-    .email()
-    .transform((val) => val.toLowerCase().trim()),
-  fullName: z.string().transform((val) => val.trim()),
+  username: z.string().toLowerCase().trim(),
+  email: z.string().email().toLowerCase().trim(),
+  fullName: z.string().trim(),
   avatar: z.string().or(z.string().url()),
   coverImage: z.string().or(z.string().url()).optional(),
   password: z.string({ message: "Password is required!" }),
 });
 
-export default userSchema;
+export { userSchema };
