735f872edf0c4ddee4908d0c84c13b1d6aaba4e1
Hinesh Miyani <hineshmiyani@gmail.com>
2024-03-08 21:00:54 +0530

    docs: add docstring to `asyncHandler` function

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,16 @@
 /* ✅ Async handler using promises */
+
+/**
+ * Wraps an asynchronous route handler or middleware function for express, automatically catching any
+ * thrown errors and passing them to the next error handling middleware in the stack.
+ *
+ * @param {Function} requestHandler - The asynchronous route handler or middleware function to be wrapped.
+ * This function should take Express's `req`, `res`, and `next` parameters.
+ *
+ * @returns {Function} A function that takes Express's `req`, `res`, and `next` parameters. When called,
+ * it executes the original `requestHandler`, and if any errors are thrown or rejected promises are returned,
+ * it catches those errors and passes them to the next error handling middleware using `next(error)`.
+ */
 const asyncHandler = (requestHandler) => {
   return (req, res, next) =>
     Promise.resolve(requestHandler(req, res, next)).catch((error) =>
