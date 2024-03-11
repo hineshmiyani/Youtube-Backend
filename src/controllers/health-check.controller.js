4b1af81619d563251a33aadffa4d51412e89fb86
Hinesh Miyani <hineshmiyani@gmail.com>
2024-03-11 12:07:11 +0530

    feat: implement health-check controller

--- src/controllers/health-check.controller.js
+++ src/controllers/health-check.controller.js
@@ -1,9 +1,9 @@
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthCheck = asyncHandler(async (req, res) => {
-  //TODO: build a healthCheck response that simply returns the OK status as json with a message
+  //✅ TODO: build a healthCheck response that simply returns the OK status as json with a message
+  res.status(200).json(new ApiResponse(200, {}, "OK"));
 });
 
 export { healthCheck };
